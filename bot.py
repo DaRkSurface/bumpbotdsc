8f0cf8fd5aa64b8e126ffa5252e0f642a3c19cdb
Darkest Surface <74876947+DaRkSurface@users.noreply.github.com>
2021-12-15 19:10:04 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,26 @@
+#####################################################
+## Discord Bump Bot                                ##
+## A bot that automatically bumps a server in dc.  ##
+## https://voidsecurity.ml                         ##
+## Coded by: drk                                   ##
+#####################################################
+
+# Imports
+import time
+from selenium import webdriver
+
+# Global Variables
+driver = webdriver.Chrome("/home/drk/Desktop/coding/python/selenium/driver/chromedriver")
+discordmail = "leonit123@gmail.com"
+discordpass = "Leonit123"
+# Code
+
+driver.get("https://discord.com/login")
+print("\n\nEntered", driver.title)
+
+elemail = driver.find_element_by_name("email")
+elemail.send_keys(discordmail)
+
+elpasswd = driver.find_element_by_name("password")
+elpasswd.send_keys(discordpass)
+
